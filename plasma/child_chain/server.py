b32e6c9beaf682d6ac59b6c0e8ef6177b0452521
yanghanbing <yanghanbing@lattebank.com>
2018-07-04 23:12:17 +0800

    init dispatcher only once

--- plasma/child_chain/server.py
+++ plasma/child_chain/server.py
@@ -27,29 +27,30 @@ def printKey(key):
     return decorator
 
 
+# Dispatcher is dictionary {<method_name>: callable}
+dispatcher["submit_block"] = lambda block: child_chain.submit_block(block)
+dispatcher["apply_transaction"] = lambda transaction: child_chain.apply_transaction(transaction)
+dispatcher["get_transaction"] = lambda blknum, txindex: child_chain.get_transaction(blknum, txindex)
+dispatcher["get_current_block"] = lambda: child_chain.get_current_block()
+dispatcher["get_current_block_num"] = lambda: child_chain.get_current_block_num()
+dispatcher["get_block"] = lambda blknum: child_chain.get_block(blknum)
+dispatcher["get_balance"] = lambda address, block: child_chain.get_balance(address, block)
+dispatcher["get_utxo"] = lambda address, block: child_chain.get_utxo(address, block)
+dispatcher["get_all_transactions"] = lambda: child_chain.get_all_transactions()
+dispatcher["apply_ps_transaction"] = lambda ps_transaction: partially_signed_transaction_pool.apply_ps_transaction(ps_transaction)
+dispatcher["get_all_ps_transactions"] = lambda: partially_signed_transaction_pool.get_all_ps_transactions()
+# MetaMask interface
+dispatcher["eth_getBalance"] = lambda address, block: child_chain.get_balance(address, block)
+dispatcher["eth_getBlockByNumber"] = lambda block, deep: child_chain.get_block_by_num(block, deep)
+dispatcher["net_version"] = lambda: child_chain.get_version()
+dispatcher["eth_sendRawTransaction"] = lambda raw_tx: child_chain.eth_raw_transaction(raw_tx)
+
+for key in dispatcher.keys():
+    dispatcher[key] = printKey(key)(dispatcher[key])
+
+
 @Request.application
 def application(request):
-    # Dispatcher is dictionary {<method_name>: callable}
-    dispatcher["submit_block"] = lambda block: child_chain.submit_block(block)
-    dispatcher["apply_transaction"] = lambda transaction: child_chain.apply_transaction(transaction)
-    dispatcher["get_transaction"] = lambda blknum, txindex: child_chain.get_transaction(blknum, txindex)
-    dispatcher["get_current_block"] = lambda: child_chain.get_current_block()
-    dispatcher["get_current_block_num"] = lambda: child_chain.get_current_block_num()
-    dispatcher["get_block"] = lambda blknum: child_chain.get_block(blknum)
-    dispatcher["get_balance"] = lambda address, block: child_chain.get_balance(address, block)
-    dispatcher["get_utxo"] = lambda address, block: child_chain.get_utxo(address, block)
-    dispatcher["get_all_transactions"] = lambda: child_chain.get_all_transactions()
-    dispatcher["apply_ps_transaction"] = lambda ps_transaction: partially_signed_transaction_pool.apply_ps_transaction(ps_transaction)
-    dispatcher["get_all_ps_transactions"] = lambda: partially_signed_transaction_pool.get_all_ps_transactions()
-    # MetaMask interface
-    dispatcher["eth_getBalance"] = lambda address, block: child_chain.get_balance(address, block)
-    dispatcher["eth_getBlockByNumber"] = lambda block, deep: child_chain.get_block_by_num(block, deep)
-    dispatcher["net_version"] = lambda: child_chain.get_version()
-    dispatcher["eth_sendRawTransaction"] = lambda raw_tx: child_chain.eth_raw_transaction(raw_tx)
-    
-    for key in dispatcher.keys():
-        dispatcher[key] = printKey(key)(dispatcher[key])
-
     response = JSONRPCResponseManager.handle(
         request.data, dispatcher)
     resp = Response(response.json, mimetype="application/json")
